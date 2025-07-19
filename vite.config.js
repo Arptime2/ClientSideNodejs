17145f7cc5a1f7b6777ef24ce7adff36cab363c2
Arptime2 <p.kohlstrunk@gmx.de>
2025-07-19 10:38:31 +0200

    gemini improved

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/client-side-nodejs-repl/',
-  server: {
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-    },
-  },
   build: {
     target: 'esnext',
   },
