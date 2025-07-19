fe5373bba9ebb339ebe0a85559f3d2ed1017c3b4
Arptime2 <p.kohlstrunk@gmx.de>
2025-07-19 10:44:08 +0200

    gemini improved

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   base: '/client-side-nodejs-repl/',
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@webcontainer/api': path.resolve(__dirname, 'node_modules/@webcontainer/api/dist/index.js'),
+    },
   },
 });
